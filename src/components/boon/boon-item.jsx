2d27950f62342d5f41738296a08aa5d32b9c2b21
biyuns <cart0089@naver.com>
2025-08-24 23:54:07 +0900

    ddfaadadd

--- src/components/boon/boon-item.jsx
+++ src/components/boon/boon-item.jsx
@@ -1,22 +1,22 @@
-import { BoonlocationLogo, BoonBoonlogo } from '../../img/img';
+// import { BoonlocationLogo, BoonBoonlogo } from '../../img/img';
 
-function BoonItem( {image, name, location, benefit}) {
-    return (
-                        <section className="boon-contant-ct">
-                    <img src={image}></img> 
-                    <div className="boon-contant-right-ct">
-                        <p> {name} </p>
-                        <div className="boon-location-ct">
-                            <img src={BoonlocationLogo}></img>
-                            <p> {location}</p>
-                        </div>
+// function BoonItem( {image, name, location, benefit}) {
+//     return (
+//                         <section className="boon-contant-ct">
+//                     <img src={image}></img> 
+//                     <div className="boon-contant-right-ct">
+//                         <p> {name} </p>
+//                         <div className="boon-location-ct">
+//                             <img src={BoonlocationLogo}></img>
+//                             <p> {location}</p>
+//                         </div>
 
-                        <div className="boon-boon-ct">
-                            <img src={BoonBoonlogo}></img>
-                            <p> {benefit} </p>
-                        </div>
-                    </div>
-                </section>
-    )
-}
-export default BoonItem;
+//                         <div className="boon-boon-ct">
+//                             <img src={BoonBoonlogo}></img>
+//                             <p> {benefit} </p>
+//                         </div>
+//                     </div>
+//                 </section>
+//     )
+// }
+// export default BoonItem;
