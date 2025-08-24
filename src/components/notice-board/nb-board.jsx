00232bc06719fc0d7e2816acaaa14372edb38a83
biyuns <cart0089@naver.com>
2025-08-25 05:49:35 +0900

    dkfjdkfdkjfkjfd

--- src/components/notice-board/nb-board.jsx
+++ src/components/notice-board/nb-board.jsx
@@ -1,102 +1,102 @@
-// src/pages/notice-board/Nbboard.jsx
+// // src/pages/notice-board/Nbboard.jsx
 
-import { Profileback, Nbstate, Nbgeul1, Nbgeul2, Nbgeul3,Nbheart, Mypagelogo
-    , Nbline, Nbcheck, Nbsubmit
- } from "../../img/img";
-import '../../components/notice-board/nb-board.css';
-function Nbboard() {
+// import { Profileback, Nbstate, Nbgeul1, Nbgeul2, Nbgeul3,Nbheart, Mypagelogo
+//     , Nbline, Nbcheck, Nbsubmit
+//  } from "../../img/img";
+// import '../../components/notice-board/nb-board.css';
+// function Nbboard() {
 
-    return (
-        <div className="total_ct">
-            <section className="nb-top-ct">
-                <img src={Profileback} alt="뒤로가기"/>
-                <p> 전체 </p>
-                <img src={Nbstate} alt="수정"/>
-            </section>
+//     return (
+//         <div className="total_ct">
+//             <section className="nb-top-ct">
+//                 <img src={Profileback} alt="뒤로가기"/>
+//                 <p> 전체 </p>
+//                 <img src={Nbstate} alt="수정"/>
+//             </section>
 
-            <div className="nb2-board-top-ct">
-                <div className="nb2-top-img-ct">
-                    <img src={'/images/img-boon/kimchi.svg'} alt="사용자사진"></img>
-                </div>
+//             <div className="nb2-board-top-ct">
+//                 <div className="nb2-top-img-ct">
+//                     <img src={'/images/img-boon/kimchi.svg'} alt="사용자사진"></img>
+//                 </div>
 
-                <div className="nb2-name-time">
-                    <p className="nb2-name"> 이름 </p>
-                    <p className="nb2-time"> 08/12 16:35</p>
-                </div>
-            </div>
+//                 <div className="nb2-name-time">
+//                     <p className="nb2-name"> 이름 </p>
+//                     <p className="nb2-time"> 08/12 16:35</p>
+//                 </div>
+//             </div>
 
-            <p className="nb2-title"> 나만 아는 을지대 숨은 맛집</p>
+//             <p className="nb2-title"> 나만 아는 을지대 숨은 맛집</p>
 
-            <p className="nb2-contant"> 맨날 학교 앞에서 뭐 먹을지 고민하면 여기 가봐. 가격 착하고, 양 품짐하고, 사장님도 친절 혼밥도 편하고, 친구들이랑 가도 딱 좋음</p>
+//             <p className="nb2-contant"> 맨날 학교 앞에서 뭐 먹을지 고민하면 여기 가봐. 가격 착하고, 양 품짐하고, 사장님도 친절 혼밥도 편하고, 친구들이랑 가도 딱 좋음</p>
 
-            <div className="nb2-user-push-img-ct">
-                <div className="nb2-user-img">
-                    <img src={'/images/img-boon/kimchi.svg'}></img>
-                </div>
-                <div className="nb2-user-img">
-                    <img src={'/images/img-boon/kimchi.svg'}></img>
-                </div>
-                <div className="nb2-user-img">
-                    <img src={'/images/img-boon/kimchi.svg'}></img>
-                </div>
-                <div className="nb2-user-img">
-                    <img src={'/images/img-boon/kimchi.svg'}></img>
-                </div>
-                <div className="nb2-user-img">
-                    <img src={'/images/img-boon/kimchi.svg'}></img>
-                </div>
-            </div>
+//             <div className="nb2-user-push-img-ct">
+//                 <div className="nb2-user-img">
+//                     <img src={'/images/img-boon/kimchi.svg'}></img>
+//                 </div>
+//                 <div className="nb2-user-img">
+//                     <img src={'/images/img-boon/kimchi.svg'}></img>
+//                 </div>
+//                 <div className="nb2-user-img">
+//                     <img src={'/images/img-boon/kimchi.svg'}></img>
+//                 </div>
+//                 <div className="nb2-user-img">
+//                     <img src={'/images/img-boon/kimchi.svg'}></img>
+//                 </div>
+//                 <div className="nb2-user-img">
+//                     <img src={'/images/img-boon/kimchi.svg'}></img>
+//                 </div>
+//             </div>
 
-            <section className="nb2-dat-heart">
-                <div className="nb2-comment-total-ct">
-                <div className="nb2-coment-ct">
-                    <img src={Nbgeul1} alt="댓글"></img>
-                    <img src={Nbgeul2} alt="댓글"></img>
-                    <img src={Nbgeul3} alt="댓글"></img>
-                </div>
-                    <p> 21 </p>
-                </div>
+//             <section className="nb2-dat-heart">
+//                 <div className="nb2-comment-total-ct">
+//                 <div className="nb2-coment-ct">
+//                     <img src={Nbgeul1} alt="댓글"></img>
+//                     <img src={Nbgeul2} alt="댓글"></img>
+//                     <img src={Nbgeul3} alt="댓글"></img>
+//                 </div>
+//                     <p> 21 </p>
+//                 </div>
 
-                <div className="nb2-heart-ct">
-                    <img src={Nbheart} alt="하트"/>
-                    <p> 3</p>
-                </div>
-            </section>
+//                 <div className="nb2-heart-ct">
+//                     <img src={Nbheart} alt="하트"/>
+//                     <p> 3</p>
+//                 </div>
+//             </section>
 
-            <div className="nb2-line"></div>
+//             <div className="nb2-line"></div>
 
-            <section className="user-comment-total-ct">
-                <div className="user-comment-ct">
-                    <div className="user-img-ct">
-                        <img src={Mypagelogo} alt="기본이미지"/>
-                    </div>
+//             <section className="user-comment-total-ct">
+//                 <div className="user-comment-ct">
+//                     <div className="user-img-ct">
+//                         <img src={Mypagelogo} alt="기본이미지"/>
+//                     </div>
 
-                    <div className="user-name-time-ct">
-                        <p> 김을지</p>
-                        <p> 08/12 17:01</p>
-                    </div>
-                </div>
-                    <p> 맛있겠다 당장 내 입으로.</p>
+//                     <div className="user-name-time-ct">
+//                         <p> 김을지</p>
+//                         <p> 08/12 17:01</p>
+//                     </div>
+//                 </div>
+//                     <p> 맛있겠다 당장 내 입으로.</p>
 
 
-                <img src={Nbline} alt="구분선"/>
+//                 <img src={Nbline} alt="구분선"/>
 
-                <div className="user-input-comment-ct">
-                    <div className="user-input-ct">
-                        <input type="text" placeholder="댓글을 입력하세요."></input>
-                        <div className="nb2-anoymouse-check-ct">
-                            <img src={Nbcheck} alt="익명체크"></img>
-                        </div>
+//                 <div className="user-input-comment-ct">
+//                     <div className="user-input-ct">
+//                         <input type="text" placeholder="댓글을 입력하세요."></input>
+//                         <div className="nb2-anoymouse-check-ct">
+//                             <img src={Nbcheck} alt="익명체크"></img>
+//                         </div>
 
-                        <div className="nb2-submit-ct">
-                            <img src={Nbsubmit} alt="전송버튼"/>
-                        </div>
-                    </div>
-                </div>
+//                         <div className="nb2-submit-ct">
+//                             <img src={Nbsubmit} alt="전송버튼"/>
+//                         </div>
+//                     </div>
+//                 </div>
                 
-            </section>
-        </div>
-    );
-}
+//             </section>
+//         </div>
+//     );
+// }
 
-export default Nbboard;
+// export default Nbboard;
